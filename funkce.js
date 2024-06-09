994280c603e69ed063c401fe625450141df2a0fc
JakubAdam28 <146720226+JakubAdam28@users.noreply.github.com>
2024-06-09 19:50:32 +0200

    final commit

---
+++ funkce.js
@@ -0,0 +1,36 @@
+function before(){
+    document.getElementById('message')
+    .innerHTML="V pokeru Texas Hold'em je cílem vytvořit nejlepší kombinaci pěti karet z celkem sedmi karet, které hráč má k dispozici (dvě vlastní a pět společných). Kombinace jsou seřazeny od nejvyšší takto: Royal Flush, Straight Flush, Four of a Kind (Poker), Full House, Flush, Straight, Three of a Kind, Two Pair, One Pair a High Card. Hra začíná povinnými sázkami (blindy) dvou hráčů vlevo od dealera. Každý hráč dostane dvě zakryté karty. Následují čtyři kola sázek: Pre-Flop, Flop, Turn a River. Na Flopu dealer vyloží tři společné karty, na Turnu čtvrtou a na Riveru pátou. Po každém kole sázek mohou hráči složit (fold), dorovnat (call) nebo navýšit sázku (raise). Na konci hry (Showdown) vyhrává hráč s nejlepší kombinací pěti karet."
+}
+    function afterr(){
+    document.getElementById('message')
+    .innerHTML="Poker může být výdělečný, pokud se hraje strategicky a s disciplínou. Profesionálové často vydělávají díky matematickému porozumění, čtení soupeřů a správnému řízení bankrollu. Naučte se základní strategie, jako jsou starting hand charts, odds a outs, blafování a pozice. Pravidelný trénink a analýza vlastních her jsou klíčové pro zlepšení a zvyšování výhernosti. Udržování správné správy bankrollu je nezbytné, aby se předešlo ztrátám a zajistila dlouhodobá úspěšnost.";
+}
+
+
+function before2(){
+    document.getElementById('message2')
+    .innerHTML="Blackjack je hra, ve které hráči soupeří s dealem. Cílem je získat co nejvyšší součet karet, aniž by se překročila hodnota 21. Hráči začínají s dvěma kartami a mohou si vzít další (hit), stát se se svým současným součtem (stand), zdvojnásobit sázku nebo rozdělit karty. Cílem je porazit dealera nebo dosáhnout blackjacku (21 bodů). Hra kombinuje strategii a štěstí a vyžaduje dobré rozhodování během hry."
+}
+    function afterr2(){
+    document.getElementById('message2')
+    .innerHTML="Celkově lze říci, že blackjack může být vydelečný, pokud použijete správnou strategii, máte dostatečnou bankroll a hrajete v kasinu s výhodnými podmínkami. Nicméně, jako u všech hazardních her, existuje vždy riziko ztráty peněz, a proto je důležité hrát zodpovědně a uvědomit si své limity.";
+}
+function before3(){
+    document.getElementById('message3')
+    .innerHTML="Ruleta je hra, ve které hráči umisťují sázky na čísla, barvy nebo skupiny čísel na hracím stole. Poté, co hráči vsadí, dealer otáčí kolo rulety a hodí kuličku do protisměrně otáčejícího se kola. Když se kulička zastaví, vyhrávají hráči sázky podle toho, na co vsadili. Pokud kulička přistane na nulu, kasino získává výhodu a hráči, kteří nevsadili na nulu, prohrávají své sázky."
+}
+    function afterr3(){
+    document.getElementById('message3')
+    .innerHTML="Ruleta je čistě šťastná hra a výsledek závisí na náhodě. Hráči nemohou ovlivnit, kde kulička přistane, většina variant rulety má house edge, což je matematická výhoda kasina nad hráčem. Například evropská ruleta má house edge přibližně 2.7 %. Z tohoto pohledu je ruleta pro hráče obvykle zisková na krátkodobém základě"
+
+}
+function before4(){
+    document.getElementById('message4')
+    .innerHTML="Sports betting je forma hazardní hry, ve které jednotlivci sázejí na výsledek sportovní události, přičemž mohou vsadit na různé aspekty, jako jsou výsledek zápasu, počet bodů, či čas prvního gólu, a výhry se udělují v souladu s pravděpodobností dané události a kurzy stanovenými sázkovou kanceláří."
+}
+    function afterr4(){
+    document.getElementById('message4')
+    .innerHTML="Sázky na sport se mohou vyplatit jako forma zábavy, ale jejich finanční výhodnost je nejistá a závisí na štěstí a znalostech o sportu. Je důležité sázet pouze s penězi, které si můžete dovolit ztratit, a mít realistická očekávání ohledně výher a ztrát."
+
+}
